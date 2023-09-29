681a8cd2d73b1c642e2fe3c17b41ae407d5ae494
Riwan <coefficriwan@gmail.com>
2023-09-29 14:47:59 +0200

    feat: count occurrence with vector method

--- TME2/main.cpp
+++ TME2/main.cpp
@@ -3,14 +3,36 @@
 #include <regex>
 #include <chrono>
 
+bool is_new_word(const std::vector<std::string>& word_vec, const std::string& word) {
+	for (const auto& w : word_vec) {
+		if (w == word) return false;
+	}
+	return true;
+}
+
+void count_occurence(std::vector<std::pair<std::string, int>>& word_occ, const std::string& word) {
+  for (auto& p : word_occ) {
+    if (p.first == word) {
+      p.second += 1;
+      return;
+    }
+  }
+  word_occ.emplace_back(word, 0);
+}
+
 int main () {
 	using namespace std;
 	using namespace std::chrono;
 
-	ifstream input = ifstream("/tmp/WarAndPeace.txt");
+	ifstream input = ifstream("../WarAndPeace.txt");
 
 	auto start = steady_clock::now();
 	cout << "Parsing War and Peace" << endl;
+
+	vector<string> word_bank;
+	size_t unique_words = 0;
+
+  vector<pair<string, int>> word_occurence;
 
 	size_t nombre_lu = 0;
 	// prochain mot lu
@@ -23,10 +45,13 @@ int main () {
 		// passe en lowercase
 		transform(word.begin(),word.end(),word.begin(),::tolower);
 
+		word_bank.push_back(word);
+		if (is_new_word(word_bank, word)) unique_words++;
+
 		// word est maintenant "tout propre"
 		if (nombre_lu % 100 == 0)
 			// on affiche un mot "propre" sur 100
-			cout << nombre_lu << ": "<< word << endl;
+			cout << nombre_lu << ": "<< word << "\n";
 		nombre_lu++;
 	}
 	input.close();
@@ -39,6 +64,8 @@ int main () {
               << "ms.\n";
 
     cout << "Found a total of " << nombre_lu << " words." << endl;
+
+	cout << "Number of unique words found with vector: " << unique_words << endl;
 
     return 0;
 }
