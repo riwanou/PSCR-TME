ae76afb4803c011fa5ae9e502a4a41a1a8efd4fa
Riwan <coefficriwan@gmail.com>
2023-10-04 19:17:27 +0200

    feat: hashmap grow, sort of top words in text

--- TME2/hashmap.h
+++ TME2/hashmap.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <algorithm>
 #include <forward_list>
+#include <iostream>
+#include <iterator>
 #include <vector>
 
 using namespace std;
@@ -9,29 +12,65 @@ template <typename K, typename V> class HashMap {
   typedef pair<K, V> entry;
 
 public:
-  HashMap(size_t size) { 
-    buckets = vector<forward_list<entry>>(size);
-    for (auto& bucket : buckets) {
-      bucket = forward_list<entry>();
-    }
-  }
+  HashMap(size_t size) { buckets = vector<forward_list<entry>>(size); }
 
-  V* get(const K& key) {
-    auto key_index = hash<K>(key) % buckets.size();
-    auto bucket = buckets[key_index];
-    for (const entry& value : bucket) {
-      if (value.first == key) return &value.second;
+  V *get(const K &key) {
+    int key_index = std::hash<K>()(key) % buckets.size();
+    forward_list<entry> &bucket = buckets[key_index];
+    for (entry &value : bucket) {
+      if (value.first == key)
+        return &value.second;
     }
     return nullptr;
   }
 
-  bool put(const K& key, const V& value) {
-    auto key_index = hash<K>(key) % buckets.size();
-    auto bucket = buckets[key_index];
+  bool put(const K &key, const V &value) {
+    int key_index = hash<K>()(key) % buckets.size();
+    forward_list<entry> &bucket = buckets[key_index];
+    // check if already exist, in this case, modify in place
+    for (entry &bucket_value : bucket) {
+      if (bucket_value.first == key) {
+        bucket_value.second = value;
+        return true;
+      }
+    }
 
+    // insert
+    bucket.emplace_front(key, value);
+
+    // resize if needed
+    size_t nb_buckets = buckets.size();
+    if (size() >= nb_buckets * 0.8)
+      grow(nb_buckets * 2);
+
+    return false;
+  }
+
+  size_t size() const {
+    size_t size = 0;
+    for (const forward_list<entry> &bucket : buckets) {
+      size += distance(bucket.begin(), bucket.end());
+    }
+    return size;
+  }
+
+  const vector<forward_list<entry>>& entries() const {
+    return buckets;
   }
 
 private:
   vector<forward_list<entry>> buckets;
 
+  // when size > 80%, resize the hashmap
+  void grow(size_t size) {
+    auto new_buckets = vector<forward_list<entry>>(size);
+    // copy all elements into new hashmap
+    for (const auto &bucket : buckets) {
+      for (const auto &value : bucket) {
+        int key_index = hash<K>()(value.first) % new_buckets.capacity();
+        new_buckets[key_index].emplace_front(value.first, value.second);
+      }
+    }
+    buckets = new_buckets;
+  }
 };
--- TME2/main.cpp
+++ TME2/main.cpp
@@ -1,86 +1,113 @@
-#include <iostream>
-#include <fstream>
-#include <regex>
 #include <chrono>
+#include <fstream>
+#include <iostream>
+#include <regex>
 #include <set>
 
 #include "hashmap.h"
 
-bool is_new_word(std::vector<std::string>& word_vec, const std::string& word) {
-	for (const auto& w : word_vec) {
-		if (w == word) return false;
-	}
+bool is_new_word(std::vector<std::string> &word_vec, const std::string &word) {
+  for (const auto &w : word_vec) {
+    if (w == word)
+      return false;
+  }
   word_vec.push_back(word);
-	return true;
+  return true;
 }
 
-void count_occurence(std::vector<std::pair<std::string, uint>>& word_occ, const std::string& word) {
-  for (auto& p : word_occ) {
+void count_occurence(std::vector<std::pair<std::string, uint>> &word_occ,
+                     const std::string &word) {
+  for (auto &p : word_occ) {
     if (p.first == word) {
       p.second += 1;
       return;
     }
   }
-  word_occ.emplace_back(word, 0);
+  word_occ.emplace_back(word, 1);
 }
 
-int main () {
-	using namespace std;
-	using namespace std::chrono;
+int main() {
+  using namespace std;
+  using namespace std::chrono;
 
-	ifstream input = ifstream("../WarAndPeace.txt");
+  ifstream input = ifstream("../WarAndPeace.txt");
 
+  HashMap<string, int> map(10);
 
-	auto start = steady_clock::now();
-	cout << "Parsing War and Peace" << endl;
+  auto start = steady_clock::now();
+  cout << "Parsing War and Peace" << endl;
 
-	vector<string> word_bank;
-	size_t unique_words = 0;
-
-  vector<pair<string, uint>> word_occurence;
+  // vector<string> word_bank;
+  // size_t unique_words = 0;
+  // vector<pair<string, uint>> word_occurence;
 
   // HashMap word_hashmap;
 
-	size_t nombre_lu = 0;
-	// prochain mot lu
-	string word;
-	// une regex qui reconnait les caractères anormaux (négation des lettres)
-	regex re( R"([^a-zA-Z])");
-	while (input >> word) {
-		// élimine la ponctuation et les caractères spéciaux
-		word = regex_replace ( word, re, "");
-		// passe en lowercase
-		transform(word.begin(),word.end(),word.begin(),::tolower);
+  size_t nombre_lu = 0;
+  // prochain mot lu
+  string word;
+  // une regex qui reconnait les caractères anormaux (négation des lettres)
+  regex re(R"([^a-zA-Z])");
+  while (input >> word) {
+    // élimine la ponctuation et les caractères spéciaux
+    word = regex_replace(word, re, "");
+    // passe en lowercase
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
 
-		if (is_new_word(word_bank, word)) unique_words++;
-    count_occurence(word_occurence, word);
+    // if (is_new_word(word_bank, word)) unique_words++;
+    // count_occurence(word_occurence, word);
+    int *nb = map.get(word);
+    if (nb) {
+      map.put(word, *nb + 1);
+    } else {
+      map.put(word, 1);
+    }
 
-		// word est maintenant "tout propre"
-		if (nombre_lu % 100 == 0)
-			// on affiche un mot "propre" sur 100
-			cout << nombre_lu << ": "<< word << "\n";
-		nombre_lu++;
-	}
-	input.close();
+    // word est maintenant "tout propre"
+    if (nombre_lu % 100 == 0)
+    // on affiche un mot "propre" sur 100
+      cout << nombre_lu << ": " << word << "\n";
+    nombre_lu++;
+  }
+  input.close();
 
-	cout << "Finished Parsing War and Peace" << endl;
+  cout << "Finished Parsing War and Peace" << endl;
 
-	auto end = steady_clock::now();
-    cout << "Parsing took "
-              << duration_cast<milliseconds>(end - start).count()
-              << "ms.\n";
+  auto end = steady_clock::now();
+  cout << "Parsing took " << duration_cast<milliseconds>(end - start).count()
+       << "ms.\n";
 
-    cout << "Found a total of " << nombre_lu << " words." << endl;
+  cout << "Found a total of " << nombre_lu << " words." << endl;
 
-	cout << "Number of unique words found with vector: " << unique_words << endl;
+  // cout << "Number of unique words found with vector: " << unique_words <<
+  // endl;
 
-  for (const auto& p : word_occurence) {
-    if (p.first == "war" || p.first == "peace" || p.first == "toto") {
-      cout << p.first << ": " << p.second << endl;
+  // for (const auto& p : word_occurence) {
+  //   if (p.first == "war" || p.first == "peace" || p.first == "toto") {
+  //     cout << p.first << ": " << p.second << endl;
+  //   }
+  // }
+
+  cout << *map.get("war") << endl;
+  cout << *map.get("peace") << endl;
+  cout << (map.get("toto") == nullptr) << endl;
+
+  cout << "Number of unique words found with hashmap: " << map.size() << endl;
+
+  vector<pair<string, int>> occur_pair(map.size());
+  for (auto bucket : map.entries()) {
+    for (auto value : bucket) {
+      occur_pair.emplace_back(value.first, value.second);
     }
   }
 
-    return 0;
+  sort(occur_pair.begin(), occur_pair.end(),
+       [](const auto &a, const auto &b) { return a.second > b.second; });
+
+  for (int i = 0; i < 10; i++) {
+    auto entry = occur_pair[i];
+    cout << entry.first << ":" << entry.second << endl;
+  }
+
+  return 0;
 }
-
-
