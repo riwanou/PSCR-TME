e928307ef2b9c54ed32352798094c32c18992119
Riwan <coefficriwan@gmail.com>
2023-12-08 13:50:23 +0100

    feat: ipc stack

--- TME7/src/Stack.h
+++ TME7/src/Stack.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstring> // size_t,memset
+#include <iostream>
+#include <semaphore.h>
 
 namespace pr {
 
@@ -9,19 +11,43 @@ namespace pr {
 template <typename T> class Stack {
   T tab[STACKSIZE];
   size_t sz;
+  sem_t s_mutex, s_read, s_write;
 
 public:
-  Stack() : sz(0) { memset(tab, 0, sizeof tab); }
+  Stack() : sz(0) {
+    memset(tab, 0, sizeof tab);
+    sem_init(&s_mutex, 1, 1);
+    sem_init(&s_write, 1, STACKSIZE);
+    sem_init(&s_read, 1, 0);
+  }
+
+  ~Stack() {
+    sem_destroy(&s_mutex);
+    sem_destroy(&s_write);
+    sem_destroy(&s_read);
+  }
 
   T pop() {
     // bloquer si vide
+    sem_wait(&s_read);
+
+    sem_wait(&s_mutex);
     T toret = tab[--sz];
+    sem_post(&s_mutex);
+
+    sem_post(&s_write);
     return toret;
   }
 
   void push(T elt) {
     // bloquer si plein
+    sem_wait(&s_write);
+
+    sem_wait(&s_mutex);
     tab[sz++] = elt;
+    sem_post(&s_mutex);
+
+    sem_post(&s_read);
   }
 };
 
--- TME7/src/prod_cons.cpp
+++ TME7/src/prod_cons.cpp
@@ -1,8 +1,14 @@
 #include "Stack.h"
+#include <fcntl.h>
 #include <iostream>
+#include <semaphore.h>
+#include <sys/fcntl.h>
+#include <sys/mman.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <vector>
+
+// Warning: does not work on macos :D
 
 using namespace std;
 using namespace pr;
@@ -22,23 +28,60 @@ void consomateur(Stack<char> *stack) {
 }
 
 int main() {
-  Stack<char> *s = new Stack<char>();
+  pid_t pp, pc;
+  void *ptr;
+  Stack<char> *s;
+  int shm_fd;
 
-  pid_t pp = fork();
+  size_t stack_size = sizeof(Stack<char>);
+
+  // init shader memory segment
+  shm_fd = shm_open("/shared_memory", O_RDWR | O_CREAT | O_EXCL, 0666);
+  if (ftruncate(shm_fd, stack_size) < 0) {
+    perror("truncate");
+    goto shm_prob;
+  }
+  if (shm_fd < 0) {
+    perror("shm_open");
+    goto shm_prob;
+  }
+
+  // load memory segment into memory
+  ptr = mmap(NULL, stack_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (ptr == MAP_FAILED) {
+    perror("mmap");
+    goto mmap_prob;
+  }
+
+  s = new (ptr) Stack<char>();
+
+  pp = fork();
   if (pp == 0) {
     producteur(s);
     return 0;
   }
 
-  pid_t pc = fork();
+  pc = fork();
   if (pc == 0) {
     consomateur(s);
     return 0;
   }
 
+  // wait for childs
   wait(0);
   wait(0);
 
-  delete s;
+  // destroy resources
+  s->~Stack();
+
+mmap_prob:
+  if (munmap(ptr, stack_size) < 0) {
+    perror("unmap");
+  }
+shm_prob:
+  if (shm_unlink("/shared_memory") < 0) {
+    perror("shm_unlink");
+  }
+
   return 0;
 }
